f24c68373ea72f1d09725751381a9a07e0e94281
Erik Kalkoken <erik.kalkoken@gmail.com>
2019-08-31 21:09:01 +0200

    enable installation with PIP

---
+++ setup.py
@@ -0,0 +1,40 @@
+import os 
+from setuptools import find_packages, setup
+
+# read the contents of your README file
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='eve-shields',
+    version='0.1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='Web service for creating Eve Online related shields through shields.io',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ErikKalkoken/eve-shields',
+    author='Erik Kalkoken',
+    author_email='erik.kalkoken@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Bottle',        
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[
+        "bottle >= 0.12.17",
+        "requests >= 2.22.0",
+    ],
+)
